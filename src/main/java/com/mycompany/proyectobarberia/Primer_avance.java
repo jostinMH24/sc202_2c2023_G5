d229aec89f417344edd055d53bafd24cf0de25e2
jostinMH24 <jostinmason255@gmail.com>
2023-06-20 19:43:52 -0600

    files to open in a java project

--- src/main/java/com/mycompany/proyectobarberia/Primer_avance.java
+++ src/main/java/com/mycompany/proyectobarberia/Primer_avance.java
@@ -2,15 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package com.mycompany.mavenproject1;
 
-import javax.swing.JOptionPane;
 
 /**
  *
  * @author kenda
  */
-public class Primer_avance {
+public class ProyectoBarberia {
 
     /**
      * @param args the command line arguments
